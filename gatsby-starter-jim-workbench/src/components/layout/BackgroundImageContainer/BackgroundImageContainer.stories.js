e7630d3e578594a8c392f9d335481c1743ebcf23
jamiejohnsonkc <jamiejohnsonkc@gmail.com>
2020-07-26 16:25:52 -0500

    pushing storybook

---
+++ gatsby-starter-jim-workbench/src/components/layout/BackgroundImageContainer/BackgroundImageContainer.stories.js
@@ -0,0 +1,13 @@
+/** @jsx jsx */
+import { jsx, Box, Styled, Text } from "theme-ui"
+import React from "react"
+import BackgroundImageContainer from "./BackgroundImageContainer"
+// import styled from "@emotion/styled"
+// import BackgroundImage from "gatsby-background-image"
+// import useBackgroundImage from "../../hooks/useBackgroundImage"
+
+export default {
+  title: "BackgroundImageContainer",
+}
+
+export const standard = () => <BackgroundImageContainer />
