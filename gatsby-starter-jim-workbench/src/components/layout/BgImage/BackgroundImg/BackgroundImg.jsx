0b41d94dd63a0ba8be5241c5014a6a7f4a900ec4
jamiejohnsonkc <jamiejohnsonkc@gmail.com>
2020-07-26 18:36:07 -0500

    Background Img completed

--- gatsby-starter-jim-workbench/src/components/layout/BgImage/BackgroundImg/BackgroundImg.jsx
+++ gatsby-starter-jim-workbench/src/components/layout/BgImage/BackgroundImg/BackgroundImg.jsx
@@ -5,7 +5,7 @@ import PropTypes from "prop-types"
 import BackgroundImage from "gatsby-background-image"
 import useBackgroundImage from "../../../hooks/useBackgroundImage"
 
-const BackgroundImg = ({ className, props }) => {
+const BackgroundImg = ({ className, props, children }) => {
   const imageData = useBackgroundImage()
 
   // Set ImageData.
@@ -30,21 +30,7 @@ const BackgroundImg = ({ className, props }) => {
         },
       }}
     >
-      <Box
-        {...props}
-        sx={{
-          p: 4,
-          width: "51%",
-        }}
-      >
-        <Styled.h1>Lorem irure reprehenderit magna nisi sunt.</Styled.h1>
-        <Text>
-          Est irure Lorem incididunt eiusmod labore dolor eiusmod elit ex
-          incididunt. Duis aliqua nulla nulla do ad laborum laboris nulla mollit
-          do dolore culpa tempor deserunt. Anim sint aute ut quis cillum
-          proident eu laborum.
-        </Text>
-      </Box>
+      {children}
     </BackgroundImage>
   )
 }
--- gatsby-starter-jim-workbench/src/components/layout/BgImage/BackgroundImg/BackgroundImg.stories.js
+++ gatsby-starter-jim-workbench/src/components/layout/BgImage/BackgroundImg/BackgroundImg.stories.js
@@ -1,3 +1,5 @@
+/** @jsx jsx */
+import { jsx, Box, Text, Styled } from "theme-ui"
 import React from "react"
 import BackgroundImg from "./BackgroundImg"
 
@@ -5,4 +7,22 @@ export default {
   title: "BackgroundImg",
 }
 
-export const standard = () => <BackgroundImg />
+export const standard = ({ children, ...props }) => (
+  <BackgroundImg {...props} className={"BgImage"}>
+    <Box
+      {...props}
+      sx={{
+        p: 4,
+        width: "51%",
+      }}
+    >
+      <Styled.h1>Lorem irure reprehenderit magna nisi sunt.</Styled.h1>
+      <Text>
+        Est irure Lorem incididunt eiusmod labore dolor eiusmod elit ex
+        incididunt. Duis aliqua nulla nulla do ad laborum laboris nulla mollit
+        do dolore culpa tempor deserunt. Anim sint aute ut quis cillum proident
+        eu laborum.
+      </Text>
+    </Box>
+  </BackgroundImg>
+)
